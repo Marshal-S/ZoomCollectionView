53034168699f8d584a2da55b6656ed46c33bc410
Marshal-S <921871381@qq.com>
2018-06-20 15:16:44 +0800

    Initial Commit

---
+++ ZoomCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZoomCollectionView
+//
+//  Created by Marshal on 2018/6/20.
+//  Copyright © 2018年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZoomCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZoomCollectionView
+//
+//  Created by Marshal on 2018/6/20.
+//  Copyright © 2018年 Marshal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
